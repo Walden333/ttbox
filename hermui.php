33e925217f0dc3c4ae52872300ee037392a36542
Walden333 <130637887+Walden333@users.noreply.github.com>
2024-07-15 20:57:17 +0800

    Create hermui.php

---
+++ hermui.php
@@ -0,0 +1,440 @@
+{
+    "class": [
+        {
+            "type_id": "1",
+            "type_name": "电影"
+        },
+        {
+            "type_id": "2",
+            "type_name": "电视剧"
+        },
+        {
+            "type_id": "3",
+            "type_name": "动漫"
+        },
+        {
+            "type_id": "4",
+            "type_name": "综艺"
+        },
+        {
+            "type_id": "27",
+            "type_name": "短剧"
+        }
+    ],
+    "filters": {
+        "1": {
+            "key": "cid",
+            "name": "分类",
+            "value": [
+                {
+                    "n": "全部",
+                    "v": "1"
+                },
+                {
+                    "n": "剧情片",
+                    "v": "6"
+                },
+                {
+                    "n": "动作片",
+                    "v": "7"
+                },
+                {
+                    "n": "冒险片",
+                    "v": "8"
+                },
+                {
+                    "n": "同性片",
+                    "v": "9"
+                },
+                {
+                    "n": "喜剧片",
+                    "v": "10"
+                },
+                {
+                    "n": "奇幻片",
+                    "v": "11"
+                },
+                {
+                    "n": "恐怖片",
+                    "v": "12"
+                },
+                {
+                    "n": "悬疑片",
+                    "v": "20"
+                },
+                {
+                    "n": "惊悚片",
+                    "v": "21"
+                },
+                {
+                    "n": "灾难片",
+                    "v": "22"
+                },
+                {
+                    "n": "爱情片",
+                    "v": "23"
+                },
+                {
+                    "n": "犯罪片",
+                    "v": "24"
+                },
+                {
+                    "n": "科幻片",
+                    "v": "25"
+                },
+                {
+                    "n": "动画电影",
+                    "v": "26"
+                },
+                {
+                    "n": "歌舞片",
+                    "v": "33"
+                },
+                {
+                    "n": "战争片",
+                    "v": "34"
+                },
+                {
+                    "n": "经典片",
+                    "v": "35"
+                },
+                {
+                    "n": "网络电影",
+                    "v": "36"
+                },
+                {
+                    "n": "其它片",
+                    "v": "37"
+                }
+            ]
+        },
+        "2": {
+            "key": "cid",
+            "name": "分类",
+            "value": [
+                {
+                    "n": "全部",
+                    "v": "2"
+                },
+                {
+                    "n": "国产剧",
+                    "v": "13"
+                },
+                {
+                    "n": "港剧",
+                    "v": "14"
+                },
+                {
+                    "n": "韩剧",
+                    "v": "15"
+                },
+                {
+                    "n": "日剧",
+                    "v": "16"
+                },
+                {
+                    "n": "泰剧",
+                    "v": "28"
+                },
+                {
+                    "n": "台剧",
+                    "v": "29"
+                },
+                {
+                    "n": "欧美剧",
+                    "v": "30"
+                },
+                {
+                    "n": "新马剧",
+                    "v": "31"
+                },
+                {
+                    "n": "其他剧",
+                    "v": "32"
+                }
+            ]
+        },
+        "3": {
+            "key": "cid",
+            "name": "分类",
+            "value": [
+                {
+                    "n": "全部",
+                    "v": "3"
+                },
+                {
+                    "n": "欧美动漫",
+                    "v": "57"
+                },
+                {
+                    "n": "日本动漫",
+                    "v": "58"
+                },
+                {
+                    "n": "韩国动漫",
+                    "v": "59"
+                },
+                {
+                    "n": "国产动漫",
+                    "v": "60"
+                },
+                {
+                    "n": "港台动漫",
+                    "v": "61"
+                },
+                {
+                    "n": "新马泰动漫",
+                    "v": "62"
+                },
+                {
+                    "n": "其它动漫",
+                    "v": "63"
+                }
+            ]
+        },
+        "4": {
+            "key": "cid",
+            "name": "分类",
+            "value": [
+                {
+                    "n": "全部",
+                    "v": "4"
+                },
+                {
+                    "n": "国产综艺",
+                    "v": "38"
+                },
+                {
+                    "n": "港台综艺",
+                    "v": "39"
+                },
+                {
+                    "n": "韩国综艺",
+                    "v": "40"
+                },
+                {
+                    "n": "日本综艺",
+                    "v": "41"
+                },
+                {
+                    "n": "欧美综艺",
+                    "v": "42"
+                },
+                {
+                    "n": "新马泰综艺",
+                    "v": "43"
+                },
+                {
+                    "n": "其他综艺",
+                    "v": "44"
+                }
+            ]
+        },
+        "27": {
+            "key": "cid",
+            "name": "分类",
+            "value": [
+                {
+                    "n": "全部",
+                    "v": "27"
+                },
+                {
+                    "n": "古装短剧",
+                    "v": "45"
+                },
+                {
+                    "n": "虐恋短剧",
+                    "v": "46"
+                },
+                {
+                    "n": "逆袭短剧",
+                    "v": "47"
+                },
+                {
+                    "n": "悬疑短剧",
+                    "v": "48"
+                },
+                {
+                    "n": "神豪短剧",
+                    "v": "49"
+                },
+                {
+                    "n": "重生短剧",
+                    "v": "50"
+                },
+                {
+                    "n": "复仇短剧",
+                    "v": "51"
+                },
+                {
+                    "n": "穿越短剧",
+                    "v": "52"
+                },
+                {
+                    "n": "甜宠短剧",
+                    "v": "53"
+                },
+                {
+                    "n": "强者短剧",
+                    "v": "54"
+                },
+                {
+                    "n": "萌宝短剧",
+                    "v": "55"
+                },
+                {
+                    "n": "其它短剧",
+                    "v": "56"
+                }
+            ]
+        }
+    },
+    "jx": 0,
+    "list": [
+        {
+            "vod_id": "7774",
+            "vod_name": "当男人恋爱时",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/856d955810d14213a7815f3c80e3d7bc.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7775",
+            "vod_name": "吐槽吐槽大会 第五季",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/d3c7c695d56d408a8713e62c0459b621.jpg",
+            "vod_remarks": "已完结"
+        },
+        {
+            "vod_id": "7779",
+            "vod_name": "江湖儿女",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/a48367732683441a839127e5fcc7bd3c.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7780",
+            "vod_name": "灰姑娘",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/98bc9476382140efacb9a17a11e26efb.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7781",
+            "vod_name": "教父3",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/0c523bd99e4e4c68915a3592d1f3c14c.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7782",
+            "vod_name": "画皮",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/25d13a61c35d4988a108df705c61d273.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7785",
+            "vod_name": "三时四餐",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/88a47046e45246b8826635adda784f04.jpg",
+            "vod_remarks": "已完结"
+        },
+        {
+            "vod_id": "7789",
+            "vod_name": "寂静之地",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/775eb65aab04413593ccb1386f331dc9.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7790",
+            "vod_name": "何以笙箫默",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/34e36cf4343940368bb2f75aa58626bc.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7791",
+            "vod_name": "记得我",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/97e4e263b0a64323a658606dfdf35563.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7793",
+            "vod_name": "中国新相亲 第一季",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/43ae38bbf3dd45dca9b17e50a5122715.jpg",
+            "vod_remarks": "已完结"
+        },
+        {
+            "vod_id": "7800",
+            "vod_name": "我在时间尽头等你",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/5d158c8c8fc0405d820d4b397186ede3.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7802",
+            "vod_name": "两杆大烟枪",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/0ebbdb7199a34d698527ee578dbb3744.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7810",
+            "vod_name": "逃出绝命镇",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/74965e03e4e446fdaaaec64603bd9503.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7811",
+            "vod_name": "好莱坞往事",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/3a88cfee1eb34c54ad5d25e7be832117.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7822",
+            "vod_name": "出租车司机",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/63ed7829ac6b477994daea621154dd4a.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7823",
+            "vod_name": "奔跑吧！兄弟",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/bf6f2009b0ae4fa782f226a8798461b3.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7824",
+            "vod_name": "悲惨世界",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/e50667249fd745ac8da7d7a4643fe0ec.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7826",
+            "vod_name": "你好生活 第二季",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/41805e8baf004fc384ab348e4397c1c7.jpg",
+            "vod_remarks": "已完结"
+        },
+        {
+            "vod_id": "7834",
+            "vod_name": "狄仁杰之神都龙王",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/b29b1f8a708a4087bf88d9c7ad530fea.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7835",
+            "vod_name": "蝴蝶效应",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/a3626035956b4c5abdd67dad00eddd03.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7841",
+            "vod_name": "X战警3：背水一战",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/feac84f40bf74153b198d2b0ce4cbd84.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7842",
+            "vod_name": "黑夜传说",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/422a94da30094c69ae4f553b103b46dc.jpg",
+            "vod_remarks": ""
+        },
+        {
+            "vod_id": "7843",
+            "vod_name": "越光宝盒",
+            "vod_pic": "https://assets.heimuer.tv/imgs/2019/03/07/32efe4c4d8e24be4bccf58085ce474bf.jpg",
+            "vod_remarks": ""
+        }
+    ],
+    "parse": 0
+}
